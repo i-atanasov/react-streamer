ed95ac05c5f3f56e7bebc1adb0dc898db31eb378
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-22 15:00:33 +0300

    redux form integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
+    <Provider store={store}>
         <App />
+    </Provider>
     );
